15988cdf20d2d0921bd1fe0bcf950fb2e16b66a7
heshamelmasry77 <heshamelmasry@outlook.com>
2017-03-15 14:40:46 +0200

    Adding meetings records to the database

--- builds/development/js/controllers/meetings.js
+++ builds/development/js/controllers/meetings.js
@@ -1,3 +1,31 @@
-myApp.controller('MeetingsController', ['$scope', function($scope) {
-    $scope.message = "Success!!!";
-}]);
+myApp.controller('MeetingsController',
+    ['$scope', '$firebaseAuth', '$firebaseArray',
+        function ($scope, $firebaseAuth, $firebaseArray) {
+
+            // create a reference to the database
+            var ref = firebase.database().ref();
+            // So this will be a reference to our database
+
+            // another variable for the authentication part.
+            var auth = $firebaseAuth();
+
+            // im going to use another method called onAuthStateChanged.
+            // This method is going to allow us to detect when a user has logged in
+            auth.$onAuthStateChanged(function (authUser) {
+                if (authUser) { // if there is an authenticated user then do the below code
+
+                    var meetingsRef = ref.child('users').child(authUser.uid).child('meetings');
+                    var meetingsInfo = $firebaseArray(meetingsRef);
+
+                    $scope.addMeeting = function () {
+                        meetingsInfo.$add({
+                            name: $scope.meetingname,
+                            date: firebase.database.ServerValue.TIMESTAMP
+                        }).then(function() {
+                            $scope.meetingname='';
+                        }); //promise
+
+                    } // addMeeting
+                } //authUser
+            }); //onAuthStateChanged
+        }]); //myApp.controller
